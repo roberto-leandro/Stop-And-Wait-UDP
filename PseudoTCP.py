9c91ea5ba45f673d20f8020d4d4081b8f2163903
Carlos Azofeifa Aguirre <b50761@ecci.ucr.ac.cr@ecci-lbl-204-13.ecci.ucr.ac.cr>
2018-10-01 12:24:29 -0600

    Agrega constantes de la encabezado de los mensajes

--- PseudoTCP.py
+++ PseudoTCP.py
@@ -1,11 +1,17 @@
 import socket
+import struct
 
 class PseudoTCPNode:
+    HEADER_SIZE = 1
     PACKET_SIZE = 1
-    SOCKET_TIMEOUT = 50
+    SOCKET_TIMEOUT = .50
+    HEADER_SYN = 0x01
+    HEADER_ACK = 0x02
+    HEADER_FIN = 0x04
 
     def __init__(self):
         self.sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+        self.sock.settimeout(PseudoTCPNode.SOCKET_TIMEOUT)
 
     def bind(self, address):
         self.sock.bind(address)
