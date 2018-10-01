712b673d40481878ce3dbec9453e78f0aadc1961
Fabian Leandro Flores <b53779@ecci.ucr.ac.cr>
2018-10-01 12:07:58 -0600

    Comentarios con la estructura de connect()

--- PseudoTCP.py
+++ PseudoTCP.py
@@ -1,14 +1,16 @@
 import socket
 
+
 class PseudoTCPNode:
     PACKET_SIZE = 1
     SOCKET_TIMEOUT = 50
 
     def __init__(self):
         self.sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+        self.sock.settimeout(self.SOCKET_TIMEOUT)
 
-    def bind(self):
-        pass
+    def bind(self, address):
+        self.sock.bind(address)
 
     def accept(self):
         pass
@@ -16,7 +18,24 @@ class PseudoTCPNode:
     def recv(self):
         pass
 
-    def connect(self):
+    def connect(self, address):
+        # Instantiate a socket to send the data
+        new_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+
+        while True:
+            # Send SYN
+            new_socket.sendall(self.HEADER_SYN)
+
+            # Wait for SYN-ACK
+            syn_ack = new_socket.recv(1)
+
+            # If nothing was received, try again
+            if not syn_ack:
+                break
+
+
+
+        # Send ACK
         pass
 
     def send(self):
