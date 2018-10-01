eb229e080750797e3f4f83f41c492b0cd25d34f2
Fabian Leandro Flores <b53779@ecci.ucr.ac.cr>
2018-10-01 13:08:09 -0600

    Avances a la implementación de connect()

--- PseudoTCP.py
+++ PseudoTCP.py
@@ -18,7 +18,7 @@ class PseudoTCPNode:
     def _are_flags_set(header, *flags):
         are_set = True
         for flag in list(flags):
-            are_set = are_set && (flag & header != 0)
+            are_set = are_set and (flag & header != 0)
         return are_set
 
     def bind(self, address):
@@ -31,7 +31,7 @@ class PseudoTCPNode:
                 continue
 
             header = received_bytes[0]
-            is_syn = self._are_flags_set(header, HEADER_SYN)
+            is_syn = self._are_flags_set(header, self.HEADER_SYN)
             if is_syn:
                 break
 
@@ -41,25 +41,34 @@ class PseudoTCPNode:
         pass
 
     def connect(self, address):
+        print(f"Trying to connect to {address}...")
+
         # Instantiate a socket to send the data
         new_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
         new_socket.settimeout(self.SOCKET_TIMEOUT)
 
+        # Build the SYN message
+        syn_message = bytearray(2)
+        syn_message[0] = self.HEADER_SYN
+
         while True:
             # Send SYN
-            new_socket.sendall(PseudoTCPNode.HEADER_SYN)
+            print("Sending SYN...")
+            new_socket.sendall(syn_message)
 
             # Wait for SYN-ACK
-            syn_ack = new_socket.recv(PseudoTCPNode.PACKET_SIZE)
+            syn_ack = new_socket.recv(PseudoTCPNode.HEADER_SIZE)
 
-            # If nothing was received or the packet was not SYN-ACK, try again
-            if not syn_ack or not self._are_flags_set(syn_ack, self.HEADER_SYN, self.HEADER_ACK):
+            # If a packet was received and it contains SYN-ACK, continue
+            if syn_ack and self._are_flags_set(syn_ack, self.HEADER_SYN, self.HEADER_ACK):  # TODO check fin is unset
+                print("Timeout! Trying again...")
                 break
 
-
-
         # Send ACK
-        pass
+        new_socket.sendall(PseudoTCPNode.HEADER_ACK)
 
     def send(self):
         pass
+
+
+
