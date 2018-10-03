6360fc9c22381e51387cba4b3ecb27dfc6ce049b
Carlos Azofeifa <cazofeifa35@gmail.com>
2018-10-02 21:36:06 -0600

    Agrega version inicial de send()

--- PseudoTCP.py
+++ PseudoTCP.py
@@ -15,7 +15,6 @@ class PseudoTCPNode:
     def __init__(self):
         self.sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
         self.sock.settimeout(PseudoTCPNode.SOCKET_TIMEOUT)
-        self.connection = None
 
     @staticmethod
     def _are_flags_set(header, *flags):
@@ -45,7 +44,7 @@ class PseudoTCPNode:
 
             header = received_message[0]
             is_syn = self._are_flags_set(header, self.HEADER_SYN) and \
-                     self._are_flags_unset(header, self.HEADER_FIN | self.HEADER_ACK)
+                self._are_flags_unset(header, self.HEADER_FIN | self.HEADER_ACK)
             if not is_syn:
                 print("did not receive SYN, retrying")
                 continue
@@ -68,10 +67,10 @@ class PseudoTCPNode:
 
             new_header = new_received_message[0]
             is_ack = self._are_flags_set(new_header, self.HEADER_ACK) and \
-                     self._are_flags_unset(new_header, self.HEADER_FIN, self.HEADER_SYN)
-            self.connection = address
+                self._are_flags_unset(new_header, self.HEADER_FIN, self.HEADER_SYN)
             if is_ack:
                 print("ACK received")
+                self.sock.connect(address)
                 break
             print("Did not receive ACK, retrying")
 
@@ -116,11 +115,49 @@ class PseudoTCPNode:
         print(f"Sending {ack_message}")
         new_socket.sendall(ack_message)
 
-    def send(self):
-        pass
+    def send(self, message):
+        frame_bit = True
+        bytes_send = 0
+
+        while bytes_send < len(message):
+            # Make a packet and send to "connected" socket
+            # TODO(Carlos): abstract the packet construction
+            packet = bytearray(self.PACKET_SIZE)
+            header = self.HEADER_FRAME_BIT if frame_bit else 0
+            packet[0] = header
+            packet[1] = message[bytes_send]
+            self.sock.send(packet)
+
+            try:
+                maybe_ack_message = self.sock.recv(2)
+            except socket.timeout:
+                print("Timed out waiting for ACK")
+                continue
+
+            # Check if ack and ack bit is correct
+            # TODO(Carlos): refactor into a macro
+            # TODO(Carlos): check for other flags
+            maybe_ack_header = maybe_ack_message[0]
+            is_ack = self._are_flags_set(maybe_ack_header, self.HEADER_ACK)
+            expected_ack_bit = self.HEADER_ACK_BIT if not frame_bit else 0
+            is_ack_bit_correct = (maybe_ack_header | self.HEADER_ACK_BIT) == expected_ack_bit
+            if not is_ack:
+                # FIXME(Carlos): In a true full duplex this shouldn't happen
+                print("Packet received is not ACK, resending packet")
+                continue
+            if not is_ack_bit_correct:
+                # FIXME(Carlos): In this case we should wait for other packet or timeout, not resend instantly
+                print("Packet received has incorrect ACK bit, possible duplicate")
+                continue
+
+            bytes_send += 1
+            frame_bit = not frame_bit
 
     def recv(self):
-        pass
+        raise NotImplementedError
+
+    def close(self):
+        raise NotImplementedError
 
 
 node = PseudoTCPNode()
